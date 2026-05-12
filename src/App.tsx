2430d70d13e327f12f988ebf80e04fc429cbca73
evitsapara <evi.tsapi@gmail.com>
2026-05-12 20:53:59 +0300

    feat: add class and functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import FunctionalComponent from "./components/FunctionalComponent.tsx";
+import ClassComponent from "./components/ClassComponent.tsx";
+import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+
 function App() {
 
   return (
       <>
+          <FunctionalComponent />
+          <ClassComponent />
+          <ArrowFunctionalComponent />
       </>
   )
 }
 
 export default App
- 
---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,6 @@
+
+const ArrowFunctionalComponent = () => {
+    const title = "I am an Arrow Funtional Component!";
+    return <h1>{title}</h1>
+}
+export default ArrowFunctionalComponent;
